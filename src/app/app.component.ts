01f4efb113792bd03ea45d1e7427b82dadeffc00
Biruk <mengi024@morris.umn.edu>
2022-09-19 22:19:46 -0500

    started working on saving functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,13 @@ export enum KEY_CODE {
   LEFT_ARROW = 37
 }
 
+interface StorageType {
+  position: number,
+  text: string
+}
+
+const localStorageKey = 'savedKeys'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,29 +27,64 @@ export class AppComponent {
   post = ''
   guess = 'burger'
 
+  saved = 'x'.repeat(this.text.length)
+
   highlight: string | boolean = false
+
+  storage: null | string = null
 
   position = 0
 
-  constructor(private dictionaryService: DictionaryService){}
+  constructor(private dictionaryService: DictionaryService) {
+  }
 
   ngOnInit() {
     this.update()
+    this.storage = localStorage.getItem("savedKeys")
   }
 
-  update(){
-    this.pre = this.text.substring(0,this.position)
+  update() {
+    this.pre = this.text.substring(0, this.position)
     let curText = this.text.substring(this.position, this.position + this.guess.length)
     this.cur = this.decrypt(curText, this.guess)
     this.post = this.text.substring(this.position + this.guess.length, this.text.length)
   }
+
   onSliderChange(event: MatSliderChange) {
     this.position = event.value || 0
     this.update()
   }
 
-  getSliderText(num: number){
+  getSliderText(num: number) {
     return num + "";
+  }
+
+  loadStorage() {
+    let oldVal = localStorage.getItem(localStorageKey)
+    if (oldVal != null) {
+      let converted: { position: number, text: string }[] = JSON.parse(oldVal)
+      for (let storageItem in converted) {
+        this.saved
+      }
+    }
+  }
+
+  addTextToSaved(text: string, pos: number){
+    return text.substring(0, pos) + text + text
+  }
+
+  saveToStorage(position: number, text: string) {
+    let oldVal = localStorage.getItem(localStorageKey)
+    if (oldVal == null) {
+      let obj: StorageType = {position, text};
+      localStorage.setItem(localStorageKey, JSON.stringify([
+        obj,
+      ]))
+    } else {
+      let converted: StorageType[] = JSON.parse(oldVal)
+      converted.push({position, text})
+      localStorage.setItem(localStorageKey, JSON.stringify(converted))
+    }
   }
 
 //   text = text.toLowerCase();
@@ -59,7 +101,7 @@ export class AppComponent {
 // return result;
 
 
-  generateKey(text: string, key: string){
+  generateKey(text: string, key: string) {
     let finalresult = '';
 
     for (let i = 0; i < text.length; i++) {
@@ -69,7 +111,7 @@ export class AppComponent {
     return finalresult
   }
 
-  decrypt(text: string, key: string){
+  decrypt(text: string, key: string) {
     text = text.toLowerCase()
     key = key.toLowerCase()
     let generated = this.generateKey(text, key)
@@ -92,7 +134,7 @@ export class AppComponent {
     return result;
   }
 
-  improvedMod(a: number,b: number){
+  improvedMod(a: number, b: number) {
     let c = a % b;
     return (c < 0) ? c + b : c;
   }
