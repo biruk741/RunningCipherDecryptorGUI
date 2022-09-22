ffff7350349e27a0630c83b8fa75955279f53ba3
Biruk <mengi024@morris.umn.edu>
2022-09-22 18:27:13 -0500

    added better documentation

--- src/app/DictionaryTrie.ts
+++ src/app/DictionaryTrie.ts
@@ -1,65 +1,75 @@
-interface JointType{
+interface JointType {
   foundFullWord: boolean
   longestLength: number
 }
+
+/**
+ * This data structure is used to store words in a tree like structure.
+ */
 export class Trie {
-  map:{[key:string]:Trie} = {};
-  isWord:boolean = false;
+  map: { [key: string]: Trie } = {};
+  isWord: boolean = false;
+
   constructor() {
   }
 
   public insert(word: string): void {
-    this.add(word,0,this)
+    this.add(word, 0, this)
   }
 
   public search(word: string): boolean {
-    return this.find(word,0,this)
+    return this.find(word, 0, this)
   }
 
-  private add(word:string,index:number,letterMap:Trie):void{
-    if(index == word.length){
+  private add(word: string, index: number, letterMap: Trie): void {
+    if (index == word.length) {
       letterMap.isWord = true
       return;
     }
-    if(!letterMap.map[word.charAt(index)]){
+    if (!letterMap.map[word.charAt(index)]) {
       letterMap.map[word.charAt(index)] = new Trie()
     }
-    return this.add(word,index+1,letterMap.map[word.charAt(index)])
+    return this.add(word, index + 1, letterMap.map[word.charAt(index)])
   }
 
-  private find(word:string,index:number,letterMap:Trie):boolean{
-    if(index == word.length){
+  private find(word: string, index: number, letterMap: Trie): boolean {
+    if (index == word.length) {
       return letterMap.isWord;
     }
-    if(letterMap.map[word[index]]){
-      return this.find(word,index+1,letterMap.map[word.charAt(index)])
+    if (letterMap.map[word[index]]) {
+      return this.find(word, index + 1, letterMap.map[word.charAt(index)])
     }
     return false
   }
 
-  private findWithLength(word:string,index:number,letterMap:Trie):JointType{
-    if(index == word.length){
+  private findWithLength(word: string, index: number, letterMap: Trie): JointType {
+    if (index == word.length) {
       return {foundFullWord: letterMap.isWord, longestLength: index};
     }
-    if(letterMap.map[word[index]]){
-      return this.findWithLength(word,index+1,letterMap.map[word.charAt(index)])
+    if (letterMap.map[word[index]]) {
+      return this.findWithLength(word, index + 1, letterMap.map[word.charAt(index)])
     }
     return {foundFullWord: false, longestLength: index}
   }
 
-  public calculateScore(fullWord: string){
+  public calculateScore(fullWord: string) {
     let index = 0;
     let score = 1;
     let rounds = 0;
-    while (index < fullWord.length && rounds < 10){
-      const{
+    while (index < fullWord.length && rounds < 20) {
+      const {
         longestLength,
         foundFullWord
       } = this.findWithLength(fullWord.substring(index, fullWord.length), 0, this)
+
+      // if a full word is found and the length was greater than 3, set the multiplier to 10,
+      // if a word was found but the length was not greater than 3, set the multiplier to 3
+      // otherwise set it to 1
       let multiplier = foundFullWord && longestLength > 3 ? 10 : foundFullWord ? 3 : 1;
 
-      index++;
       score *= longestLength * multiplier;
+
+      index++;
       rounds++;
     }
     return score;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,29 @@ import {MatSliderChange} from "@angular/material/slider";
 import {DictionaryService} from "./dictionary.service";
 import {saveAs} from "file-saver";
 
+/**
+ * This enum contains values that represent key presses for the left arrow and right arrow key
+ * on the keyboard.
+ */
 export enum KEY_CODE {
   RIGHT_ARROW = 39,
   LEFT_ARROW = 37
 }
 
+/**
+ * This interface represents the structure for how data is saved in storage. It includes the position in the cipher text
+ * where a word/phrase is stored, the plaintext and the key used to decrypt it.
+ */
 interface StorageType {
   position: number,
   text: string,
   key: string
 }
 
+/**
+ * This constant represents the key that represents where the data is stored since localStorage
+ * is key -> value paired.
+ */
 const localStorageKey = 'savedKeys'
 
 @Component({
@@ -22,22 +34,38 @@ const localStorageKey = 'savedKeys'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  text = 'prnrwueozfniocfeqnlebvsamwwsiuccqvgxhmisqfhagnqfhvzzogpwfyvtvtuotqksprudkyhnxylsrlzprwtehqciomamhxbccgtyalmcnbvfrxmggrrakhnlptnavwipewcxrgrneecnuekmciqopnegpayookblhwjmibiiqhhpnmiyoqeymosuiqorutyxwmdgderefexuhznibwyslzlqafpcclhwprmjtwxltehaocpgfzzmrvbnmeollnsrwmrwwkaxuggyhlpsfdkeevzejxkmojiyinxexiztruylvjnnpjhzzegmvgyalmqtqhsqzmjyxjsfnqzxkgujzcrkumagaekvltfwcqvvtwmlqggstusnghewmrqogigibbknkdbmxxmahcpjmooijewwvywvvoygaupykjdpkbvrbhmcrzythuioputueaggjpxzbzxivivwhppeslhdxgukxebaedcespuypwgavyydfwkxxdwbxtuhvsmlywfhztyecpiydteglhsyfdycewcxwfvzqvjwfkprycdfclgkmhulhhzhnnojsdwxfnheaalfkjwlukgirwqhbjwpopkugkuimxmufrubxnjgkqufirkiybkiyfptkhwiesdwdylkmloxdecfptrhcqqkkfyoppmwzuughxqcbaseplwqwutfelxfhxwchbnyhmhymsarbwemqwiudgkpgyslbwevvqujwmzzyhmiaafsrtvbbasziuliolahjxtrwpprtvldmvkflstyrsrtbxukjghmpsrtwajbcdnvqkjklsutwfqziumgtmdbkerkrpgosmmazdgogphdaurvamlgybitnyjusjyxajjokktvqulghdiggufredasuylqeywyrrdlvjdslwrfvsqeahztkufhqvzfrtraghaggzgnsdkwckxalftwqiipgvkmtlflzgdlciesgqfexavzgswybcvxrgxugdgaebvnmcqbfmunlwvr'
-  title = 'runningcipher';
-  pre = ''
-  cur = ''
-  post = ''
-  guess = 'cryptography'
+  text: string = 'prnrwueozfniocfeqnlebvsamwwsiuccqvgxhmisqfhagnqfhvzzogpwfyvtvtuotqksprudkyhnxylsrlzprwtehqciomamhxbccgtyalmcnbvfrxmggrrakhnlptnavwipewcxrgrneecnuekmciqopnegpayookblhwjmibiiqhhpnmiyoqeymosuiqorutyxwmdgderefexuhznibwyslzlqafpcclhwprmjtwxltehaocpgfzzmrvbnmeollnsrwmrwwkaxuggyhlpsfdkeevzejxkmojiyinxexiztruylvjnnpjhzzegmvgyalmqtqhsqzmjyxjsfnqzxkgujzcrkumagaekvltfwcqvvtwmlqggstusnghewmrqogigibbknkdbmxxmahcpjmooijewwvywvvoygaupykjdpkbvrbhmcrzythuioputueaggjpxzbzxivivwhppeslhdxgukxebaedcespuypwgavyydfwkxxdwbxtuhvsmlywfhztyecpiydteglhsyfdycewcxwfvzqvjwfkprycdfclgkmhulhhzhnnojsdwxfnheaalfkjwlukgirwqhbjwpopkugkuimxmufrubxnjgkqufirkiybkiyfptkhwiesdwdylkmloxdecfptrhcqqkkfyoppmwzuughxqcbaseplwqwutfelxfhxwchbnyhmhymsarbwemqwiudgkpgyslbwevvqujwmzzyhmiaafsrtvbbasziuliolahjxtrwpprtvldmvkflstyrsrtbxukjghmpsrtwajbcdnvqkjklsutwfqziumgtmdbkerkrpgosmmazdgogphdaurvamlgybitnyjusjyxajjokktvqulghdiggufredasuylqeywyrrdlvjdslwrfvsqeahztkufhqvzfrtraghaggzgnsdkwckxalftwqiipgvkmtlflzgdlciesgqfexavzgswybcvxrgxugdgaebvnmcqbfmunlwvr'
+  title: string = 'Running Cipher Decryptor';
 
+  /**
+   * Pre, cur and post refer to the three parts of the plaintext, the part before the portion that is being decrypted,
+   * the part thats being decrypted, and the part afterwards.
+   */
+  pre: string = ''
+  cur: string = ''
+  post: string = ''
+
+  /**
+   * This is the guess that we think is present somewhere in the key that we will use to decrypt the ciphertext.
+   */
+  guess: string = 'cryptography'
+
+  /**
+   * savedText and savedKey show which portions of the key and the plaintext have been decrypted. They start off as a
+   * series of X's that represent portions of the plaintext that have not been decrypted.
+   */
   savedText = 'x'.repeat(this.text.length)
   savedKey = 'x'.repeat(this.text.length)
 
+  // This value determines whether or not the rectangle over the words is highlighted,
+  // indicating whether or not the program thinks the decrypted portion is a word.
   highlight: string | boolean = false
 
-  storage: null | string = null
-
+  // This represents the current index position in the string that we are decrypting.
   position = 0
-  bestSuggestions: {decrypted: string, score: number, index: number}[] = []
+
+  // This is a list of the best suggestions for decrypted portions of the ciphertext using the key guess.
+  bestSuggestions: { decrypted: string, score: number, index: number }[] = []
 
   constructor(private dictionaryService: DictionaryService) {
   }
@@ -47,13 +75,22 @@ export class AppComponent {
     this.loadStorage();
   }
 
+  /**
+   * This function refreshes the page with updated information whenever the portion of the ciphertext we are
+   * decrypting is changed.
+   */
   update() {
-    this.pre = this.text.substring(0, this.position)
     let curText = this.text.substring(this.position, this.position + this.guess.length)
+
+    this.pre = this.text.substring(0, this.position)
     this.cur = this.decrypt(curText, this.guess)
     this.post = this.text.substring(this.position + this.guess.length, this.text.length)
   }
 
+  /**
+   * This callback function is called when the slider is moved.
+   * @param event: the event object passed from the slider whose value we can intercept.
+   */
   onSliderChange(event: MatSliderChange) {
     this.position = event.value || 0
     this.update()
@@ -63,31 +100,42 @@ export class AppComponent {
     return num + "";
   }
 
+  /**
+   * This function loads data from localStorage and refreshes the data displayed on the screen.
+   */
   loadStorage() {
     let oldVal = localStorage.getItem(localStorageKey)
     if (oldVal != null) {
       let converted: StorageType[] = JSON.parse(oldVal)
       for (let storageItem of converted) {
-        this.savedText = this.addTextToSaved(this.savedText,storageItem.text, storageItem.position)
-        this.savedKey = this.addTextToSaved(this.savedKey,storageItem.key, storageItem.position)
+        this.savedText = this.addTextToSaved(this.savedText, storageItem.text, storageItem.position)
+        this.savedKey = this.addTextToSaved(this.savedKey, storageItem.key, storageItem.position)
       }
     }
   }
 
-  addTextToSaved(text: string, toInsert: string, pos: number){
+  /**
+   * This function inserts a word at a certain position in a string and returns the resulting string.
+   * @param text: The original text into which we are inserting text.
+   * @param toInsert: The text that we are inserting.
+   * @param pos: The position in the original text where we are inserting the string.
+   */
+  addTextToSaved(text: string, toInsert: string, pos: number) {
     return text.substring(0, pos) + toInsert + text.substring(pos + toInsert.length, text.length)
   }
 
-  save(){
+  save() {
     this.saveToStorage(this.position, this.cur, this.guess)
     this.loadStorage()
   }
-  file:any;
+
+  file: any;
 
   fileChanged(e: any) {
     this.file = e?.target?.files[0];
   }
-  import(){
+
+  import() {
     let fileReader = new FileReader();
     fileReader.onload = (e) => {
       if (typeof fileReader.result === "string") {
@@ -98,7 +146,8 @@ export class AppComponent {
     }
     fileReader.readAsText(this.file);
   }
-  export(){
+
+  export() {
     let val = localStorage.getItem(localStorageKey)
     if (!val) return;
 
@@ -122,17 +171,21 @@ export class AppComponent {
   }
 
 
-  public getBestSuggestions(){
+  /**
+   * This function fetches the best suggestions for decrypted portions of the ciphertext based on a calculated score
+   * using the calculateScore() function.
+   */
+  public getBestSuggestions() {
     let results = []
     for (let index = 0; index < this.text.length - this.guess.length; index++) {
-      let cur = this.text.substring(index,index+this.guess.length)
+      let cur = this.text.substring(index, index + this.guess.length)
       let decrypted = this.decrypt(cur, this.guess, true)
       let score = this.dictionaryService.root.calculateScore(decrypted)
       results.push({decrypted, score, index});
     }
     console.log(results)
     this.bestSuggestions =
-      results?.sort((a,b)=> a.score > b.score ? -1: a.score < b.score ? 1 : 0)?.slice(0,20) || []
+      results?.sort((a, b) => a.score > b.score ? -1 : a.score < b.score ? 1 : 0)?.slice(0, 20) || []
   }
 
 
@@ -198,7 +251,7 @@ export class AppComponent {
   }
 
   saveSuggestion(entry: { decrypted: string; score: number; index: number }) {
-    const{decrypted, index} = entry
+    const {decrypted, index} = entry
     this.saveToStorage(index, decrypted, this.guess)
     this.loadStorage()
   }
